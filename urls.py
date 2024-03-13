b0f498c68e825ef2ad1605983848edb849ef07dd
tanishqkatre <163336674+tanishqkatre@users.noreply.github.com>
2024-03-13 22:53:17 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,15 +1,26 @@
-from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, RegisterPage
-from django.contrib.auth.views import LogoutView
+"""myweb URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', TaskList.as_view(), name="task"),
-    path('task-create/', TaskCreate.as_view(), name="task-create"),
-    path('login/', CustomLoginView.as_view(), name="login"),
-    path('logout/', LogoutView.as_view(next_page='login'), name="logout"),
-    path('register/', RegisterPage.as_view(), name="register"),
-    path('task/<int:pk>/', TaskDetail.as_view(), name="tasks-detail"),
-    path('task-update/<int:pk>/', TaskUpdate.as_view(), name="tasks-update"),
-    path('task-delete/<int:pk>/', TaskDelete.as_view(), name="tasks-delete")
-
+    path('admin/', admin.site.urls),
+    path('', include('todo.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
